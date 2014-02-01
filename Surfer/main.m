885ce9d488edbdcb478b69adb5d58861747998ca
Faturita <rramele@gmail.com>
2014-01-31 21:15:05 -0300

    Initial Commit

---
+++ Surfer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Surfer
+//
+//  Created by Rodrigo Ramele on 31/01/14.
+//  Copyright (c) 2014 Baufest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "NSSurferAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([NSSurferAppDelegate class]));
+    }
+}
